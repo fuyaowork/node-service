0d4ed8ae305094a719fe2601f6e5298932d13486
fuyao <1204475697@qq.com>
2018-04-09 09:31:26 +0800

    a simple node service

---
+++ app.js
@@ -0,0 +1,12 @@
+//引入express中间件
+var express = require('express');
+var app = express();
+var path = require('path');
+
+app.use(express.static(path.join(__dirname, 'dist')));
+
+
+//监听端口为3000
+var server = app.listen(3000, function () {
+    console.log('Example app listening at http://localhost:3000');
+});
